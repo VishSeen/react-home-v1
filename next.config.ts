a297db4aa883dd72dc8b58b7ea5c484010e623fe
VishSeen <vishseenarain@gmail.com>
2026-02-08 06:41:50 +0400

    feat: add github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "/react-home-v1";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGitHubPages ? repoName : "",
+  assetPrefix: isGitHubPages ? repoName : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "images.unsplash.com" },
